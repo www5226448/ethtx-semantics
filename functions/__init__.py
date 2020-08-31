32d877c689ac2f61956df99f672a592dc5c0399e
Filip Małachowicz <filip.malachowicz@gmail.com>
2020-08-31 14:39:21 +0200

    Merge global functions into one file

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,1 +1,7 @@
-from .known_address_registry import known_address_registry
+def number_of_decimals(know_addresses_registry, _transaction, contract_address, default=18):
+    if contract_address is not None:
+        known_address, contract, address_info = know_addresses_registry.register(contract_address)
+        if address_info and address_info['coin'] and address_info['coin']['dec']:
+            return address_info['coin']['dec']
+
+    return default
